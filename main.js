7153982600637741d00bbf8b4404d513ae4b0fbf
Crystal <crystal.harpstreit@gmail.com>
2014-12-03 22:33:00 -0800

    Changes to js file

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ var container = document.querySelector(".container");
 
 var names = ["Eddy", "Jim", "Casey", "Laurie", "Ace"];
 
-var arr = [];
+// how will i attach my random colors to my spans?
+function colorizer() {
+  var color1 = Math.floor(Math.random() * 255);
+  var color2 = Math.floor(Math.random() * 255);
+  var color3 = Math.floor(Math.random() * 255);
 
-var whiskerLength = Math.floor(Math.random() * 101);
+  return "rgba(" + color1 + "," + color2 + "," + color3 + ",0.1)";
+}
+
+console.log(colorizer());
+
+window.colorizer = colorizer;
 
 function Cat(name, age, color, whiskerLength, bib) {
   this.name = name;
@@ -29,17 +38,44 @@ function Cat(name, age, color, whiskerLength, bib) {
 var catOne = new Cat("Eddy", 3, "TuxiBoi", 20, true);
 
 function render() {
-  for (var i = 0; i < names.length; i++) {
+  namer();
+  whiskerMkr();
+};
 
+function namer() {
+  for (var i = 0; i < names.length; i++) {
     var box = document.createElement("span");
     box.className = "draggie-box";
     container.appendChild(box);
-    box.innerHTML = names[i];
-    var draggie = new Draggabilly(box);
+
     box.dataset.property = "name";
     box.dataset.value = names[i];
+  };
+
+  var draggie = new Draggabilly(box);
+
+  draggie.on('dragStart', function(instance, event, pointer) {
+    instance.element.classList.add('active');
+  });
+
+  draggie.on('dragEnd', function(instance, event, pointer) {
+    instance.element.classList.remove('active');
+    console.log(instance.element.dataset.property);
+    console.log(instance.element.dataset.value);
+  });
+}
+
+function whiskerMkr() {
+  for (var i = 0; i < names.length; i++) {
+    var box = document.createElement("span");
+    box.className = "draggie-box";
+    container.appendChild(box);
+
+    var whiskerLength = Math.floor(Math.random() * 101);
     box.dataset.property = "whiskerLength";
     box.dataset.value = whiskerLength;
+
+    var draggie = new Draggabilly(box);
 
     draggie.on('dragStart', function(instance, event, pointer) {
       instance.element.classList.add('active');
@@ -51,6 +87,6 @@ function render() {
       console.log(instance.element.dataset.value);
     });
   }
-};
+}
 
 render();
